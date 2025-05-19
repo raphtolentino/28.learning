1594474ad39b7a26028417789e81fe31f74b3f73
Raphael Tolentino <68666795+raphtolentino@users.noreply.github.com>
2025-05-19 10:08:34 +0100

    change

--- code/bash/week 2/battery.sh
+++ code/bash/week 2/battery.sh
@@ -2,8 +2,9 @@
 
 BATTERY_LEVEL=75
 
-if [ "$BATTERY_LEVEL" -lt 20 ]; then
+if [ "$BATTERY_LEVEL" -lt 50 ]; then
     echo "Battery is low!"
 else
+    ["$BATTERY_LEVEL" -lt 20]; then
     echo "Battery is OK."
 fi
